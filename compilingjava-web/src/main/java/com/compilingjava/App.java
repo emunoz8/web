efd93f40fccbc6c168fbee1b4c48c827af82220d
Edwin <emunoz9@neiu.edu>
2025-07-25 16:54:06 -0500

    Initial commit

---
+++ compilingjava-web/src/main/java/com/compilingjava/App.java
@@ -0,0 +1,13 @@
+package com.compilingjava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
