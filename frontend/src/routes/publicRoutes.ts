5d28675cfc323d2a45ee638f20d38dc42d4ff552
Edwin <emunoz9@neiu.edu>
2025-08-02 12:01:49 -0500

    activated About page

--- frontend/src/routes/publicRoutes.ts
+++ frontend/src/routes/publicRoutes.ts
@@ -1,5 +1,5 @@
 import Home from "../pages/Home";
-// import About from "../pages/About";
+import About from "../pages/About";
 // import Projects from "../pages/Projects";
 // import Contact from "../pages/Contact";
 // import Login from "../pages/Login";
@@ -7,7 +7,7 @@ import { PublicRoute } from "./types";
 
 export const publicRoutes: PublicRoute[] = [
   { to: "/", label: "Home", Component: Home },
-  //{ to: "/about", label: "About", Component: About },
+  { to: "/about", label: "About", Component: About },
   //{ to: "/projects", label: "Projects", Component: Projects },
   //{ to: "/contact", label: "Contact", Component: Contact },
   //{ to: "/login", label: "Login", Component: Login },//work in progress
