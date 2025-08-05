e5e94aa45198a6f64f01c473151ff8589a45a65c
Edwin <emunoz9@neiu.edu>
2025-08-05 11:20:21 -0500

    removed unneccesary directory

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from "./components/NavBarComp/Navbar";
 import AppRoutes from "./routes/AppRoutes";
+import './styles/index.css';
 
 function App() {
   return (
-    <div className="code-theme">
+    <div className="code-theme" >
+      <div className="body-main">
       <Router>
         <Navbar />
         <AppRoutes />
       </Router>
+       </div>
     </div>
   );
 }
