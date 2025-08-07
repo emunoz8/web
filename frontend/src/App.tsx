30d92be1bbd243e3525c000402a4b01d64a74352
Edwin <emunoz9@neiu.edu>
2025-08-07 09:11:49 -0500

    updated Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,13 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import Navbar from "./components/layout/Navbar";
-import AppRoutes from "./routes/AppRoutes";
 import './styles/index.css';
 import { ThemeProvider } from "./context/ThemeProvider";
+import Theme from "./context/Theme";
 
 function App() {
   return (
     <ThemeProvider>
-      <div className="code-theme">
-        <div className="body-main">
-          <Router>
-            <Navbar />
-            <AppRoutes />
-          </Router>
-        </div>
-      </div>
+      <Theme/>
     </ThemeProvider>
-  
   );
 }
 
